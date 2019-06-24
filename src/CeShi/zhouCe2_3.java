4e7c0206bc68b07dd0e78297e4bc9cf4b8358fe8
plj8157 <531765902@qq.com>
2019-06-24 15:39:19 +0800

    改错

--- src/CeShi/zhouCe2_3.java
+++ src/CeShi/zhouCe2_3.java
@@ -4,11 +4,14 @@ public class zhouCe2_3 {
     public static void main(String[] args){
         int sum=0;
         int num=2;
-        int i=1;
+//        int i=2;
         do{
-            sum=i*(i-1);
+//            sum=i*(i-1);
+//            num=num+2;
+//            i++;
+            sum=sum+num;
             num=num+2;
-            i++;
+
 
         }while (num<=50);
         System.out.println("50以内的偶数的和为："+sum);
